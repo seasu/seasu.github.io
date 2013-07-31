4ce02a1a37f7143e15a3b76743f2d4e3c46033e1
Seasu <seasuwang@gmail.com>
2013-07-31 15:06:59 +0800

    add background

--- js/controllers.js
+++ js/controllers.js
@@ -4,6 +4,11 @@
 
 angular.module('improvisation365.controllers', []).
     controller('Section1Ctrl', function($scope) {
+    	$scope.totalDays = 365;
+    	$scope.finishDays = 80;
+    	$scope.progressPercent = ($scope.finishDays/$scope.totalDays)*100;
+    	$scope.progressPercent = 22;
+    	// alert($scope.progressPercent);
         $scope.start = function(){
         	alert('start click!');
         };
